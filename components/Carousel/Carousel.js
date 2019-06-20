9962b21aa287de7762ea511dcc151fd0245a0ff7
TheIllusiveC4 <gasingdong@gmail.com>
2019-06-20 15:58:38 -0700

    finish arrows for carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,14 +1,40 @@
 class Slideshow {
-  constructor(element) {
+  constructor(element, duration) {
     this.element = element;
+    // How long each slide will stay before moving on to the next
+    this.duration = duration;
     this.currentSlideIndex = 0;
     this.slides = Array.from(this.element.querySelectorAll('.slide')).map(
       slide => new Slide(slide, this)
     );
-    // this.prevButton = this.element.querySelector('.slide-arrow-prev');
-    // this.nextButton = this.element.querySelector('.slide-arrow-next');
-    // this.prevButton.addEventListener('click', () => this.nextSlide());
-    // this.nextButton.addEventListener('click', () => this.prevSlide());
+    // Controllers for the prev/next buttons
+    this.prevButton = this.element.querySelector('.slide-arrow-prev');
+    this.nextButton = this.element.querySelector('.slide-arrow-next');
+    this.prevButton.addEventListener('click', () => {
+      this.prevSlide();
+      this.resetTimer();
+    });
+    this.nextButton.addEventListener('click', () => {
+      this.nextSlide();
+      this.resetTimer();
+    });
+    this.timer = this.startTimer();
+  }
+
+  // Resets the timer, mainly for pressing the next/prev buttons so the timer won't conflict with the user
+  resetTimer() {
+    clearInterval(this.timer);
+    this.timer = this.startTimer();
+  }
+
+  startTimer() {
+    return setInterval(
+      () =>
+        slideshows.forEach(slideshow => {
+          slideshow.nextSlide();
+        }),
+      this.duration
+    );
   }
 
   nextSlide() {
@@ -46,12 +72,5 @@ class Slide {
 }
 
 const slideshows = Array.from(document.querySelectorAll('.slide-show')).map(
-  ss => new Slideshow(ss)
-);
-setInterval(
-  () =>
-    slideshows.forEach(slideshow => {
-      slideshow.nextSlide();
-    }),
-  5000
+  ss => new Slideshow(ss, 10000)
 );
